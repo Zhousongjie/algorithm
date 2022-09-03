9401c8ab62bb60b01cc5762db1cb57b2c1ea2f1d
zhousongjie <865687433@qq.com>
2022-09-03 09:02:38 +0800

    B_1001

---
+++ PAT/B_1001.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, step = 0;
+    cin >> n;
+    while (n != 1)
+    {
+        if (n % 2)
+        {
+            n = (3 * n + 1) / 2;
+        }
+        else
+        {
+            n = n / 2;
+        }
+        step++;
+    }
+    cout << step;
+}
